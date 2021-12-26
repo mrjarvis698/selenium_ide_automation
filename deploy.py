f1f06f903f690fa26ee2432f8aa385daa9cd98fa
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-12-26 23:14:21 +0530

    test: basic selenium test template
    Signed-off-by: mrjarvis698 <dudamajaykumar59@gmail.com>

--- deploy.py
+++ deploy.py
@@ -1,7 +1,43 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 import unittest
 
 class MainTestCase(unittest.TestCase):
-    print("Basic Test Template")
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path=r'chromedriver.exe')
+        self.driver.implicitly_wait(10)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("<Enter Base/Starting URL>")
+
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
